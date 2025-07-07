4e23a225a75cbc7da0b95a8d1c8c5e3ee2b4fdca
nish-r-k <131425233+nish-r-k@users.noreply.github.com>
2025-07-07 17:18:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public')); // Serve your HTML!
+app.use(express.static('public')); // for HTML!
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
